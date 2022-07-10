84107a9e873d6582c3ea07edf2ebb454106e9b98
mtsandov <mtsandov@espol.edu.ec>
2022-07-10 14:58:12 -0500

    Cambios en VtBiblioteca, agregando motor de búsqueda por personas y lugares

--- proyectoEstructuras/src/main/java/ec/edu/espol/proyectoestructuras/VtBibliotecaController.java
+++ proyectoEstructuras/src/main/java/ec/edu/espol/proyectoestructuras/VtBibliotecaController.java
@@ -12,11 +12,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -36,21 +38,22 @@ public class VtBibliotecaController implements Initializable {
      * Initializes the controller class.
      */
     @FXML
-    private ComboBox<?> cbx1;
+    private ComboBox<String> cbx1;
 
-    @FXML
-    private ComboBox<?> cbx2;
+
 
     @FXML
     private VBox fotos= new VBox();
 
     @FXML
     private ImageView volver;
+    @FXML
+    private TextField lugar;
 
     @FXML
-    void buscarAutos(MouseEvent event) {
+    private TextField personas;
 
-    }
+    
 
     @FXML
     void regresar(MouseEvent event) throws IOException {
@@ -59,7 +62,9 @@ public class VtBibliotecaController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        buscarFotos();
+        ObservableList<String> tipos = FXCollections.observableArrayList("Lugar","Personas","Personas y Lugar");
+   
+        cbx1.setItems(tipos);
     }
 
     // Se necesita crear una lista de Fotos en donde se guarden las fotos subidas, despues el código debería mostrar todas las fotos al dar clik en
@@ -67,30 +72,73 @@ public class VtBibliotecaController implements Initializable {
     @FXML
     private void buscarFotos() {
         fotos.getChildren().clear();
+        LinkedList<Foto> filtrada = new LinkedList<>();
+        LinkedList<Foto> filtrada2 = new LinkedList<>();
+        LinkedList<Foto> filtrada3 = new LinkedList<>();
+
 
         LinkedList<Foto> albumes = Foto.leer("fotos2.txt");
+        
         System.out.println(albumes.getSize());
         System.out.println("holaMundo");
         
         System.out.println(albumes.getHead());
         System.out.println(albumes.getSize());
         ScrollPane sp= new ScrollPane();
-        for (int i = 0; i < albumes.getSize()-1; i++) {
-            HBox datosFoto = new HBox();
-            System.out.println(albumes.get(i).getRutaFoto());
-            Text t = new Text(albumes.get(i).getDescripcion());
-            datosFoto.setSpacing(10);
-            Image img = new Image(albumes.get(i).getRutaFoto());
-            ImageView imgview = new ImageView(img);
-            imgview.setFitWidth(150);
-            imgview.setFitHeight(150);
-            datosFoto.getChildren().add(t);
-            datosFoto.getChildren().add(imgview);
-            sp.setContent(datosFoto);
-            //fotos.getChildren().add(sp);
-            fotos.getChildren().add(datosFoto);
-
+        if((String)cbx1.getValue() == null){
+             for (int i = 0; i < albumes.getSize()-1; i++) {
+                 filtrada.addLast(albumes.get(i));
+            
         }
+                
+                }else if("Lugar".equals((String)cbx1.getValue())){
+                    for (int i = 0; i < albumes.getSize()-1; i++){
+                        if(albumes.get(i).getLugar().equals(lugar.getText())){
+                            filtrada.addLast(albumes.get(i));
+                        }
+                        }
+                }else if("Personas".equals((String)cbx1.getValue())){
+                    for (int i = 0; i < albumes.getSize()-1; i++){
+                        if(albumes.get(i).getPersonas().equals(personas)){
+                            filtrada.addLast(albumes.get(i));
+                        }
+                        }
+                }else if("Personas y Lugar".equals((String)cbx1.getValue())){
+                    for (int i = 0; i < albumes.getSize()-1; i++){
+                        if(albumes.get(i).getPersonas().equals(personas) && albumes.get(i).getLugar().equals(lugar.getText()) ){
+                            filtrada.addLast(albumes.get(i));
+                }
+                }
+                    
+                    
+                }
+        for (int i = 0; i < filtrada.getSize()-1; i++) {
+        HBox datosFoto = new HBox();
+        System.out.println(filtrada.get(i).getRutaFoto());
+        Text t = new Text(filtrada.get(i).getDescripcion());
+        datosFoto.setSpacing(10);
+        Image img = new Image(filtrada.get(i).getRutaFoto());
+        ImageView imgview = new ImageView(img);
+        imgview.setFitWidth(150);
+        imgview.setFitHeight(150);
+        datosFoto.getChildren().add(t);
+        datosFoto.getChildren().add(imgview);
+        sp.setContent(datosFoto);
+            //fotos.getChildren().add(sp);
+        fotos.getChildren().add(datosFoto);
+        System.out.println(cbx1.getValue());
+        System.out.println(lugar.getText());
+        }
+        
+            
+            
+         
+    
+    /*public static void main(String[] args) {
+        VtBibliotecaController v = new VtBibliotecaController();
+        v.buscarFotos();
+    }*/
+}
 
     }
     
@@ -98,4 +146,4 @@ public class VtBibliotecaController implements Initializable {
         VtBibliotecaController v = new VtBibliotecaController();
         v.buscarFotos();
     }*/
-}
+
